29d74dac577fe1eef180841ac5c012bd6186ef59
Saurabh Chatterjee <119055623+chatterjeesaurabh@users.noreply.github.com>
2024-08-07 17:12:29 +0530

    Add files via upload

--- Face Action Unit Intensity Estimation/AU_Recognition/models/resnet18.py
+++ Face Action Unit Intensity Estimation/AU_Recognition/models/resnet18.py
@@ -8,17 +8,17 @@ class ResNet18(nn.Module):
 		super(ResNet18, self).__init__()
 		self.fm_distillation = opts.fm_distillation
 		self.dropout = opts.dropout
-		resnet18 = torchvision.models.resnet18(pretrained=True)
+		resnet18 = torchvision.models.resnet18(pretrained=True)		# ResNet-18 load (TorchVision Pre-Trained)
 		resnet18_layers = list(resnet18.children())[:-1]
-		self.encoder = nn.Sequential(*resnet18_layers)
+		self.encoder = nn.Sequential(*resnet18_layers)				# Encoder = ResNet-18
 
-		self.classifier = nn.Sequential(
+		self.classifier = nn.Sequential(							# next Linear Classifier Layer
 					nn.Linear(512, 128),
 					nn.ReLU(),
 					nn.BatchNorm1d(num_features=128),
 					nn.Dropout(p=self.dropout),
-					nn.Linear(128, opts.num_labels),
-					nn.Sigmoid()
+					nn.Linear(128, opts.num_labels),				# Output Dim =  12
+					nn.Sigmoid()									# each of the 12 output value range : 0-1 **  (later needs to be multiplied by 5)
 					)
 				
 
